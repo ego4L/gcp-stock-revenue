c7dbb1dd9f7507d0f2d73df0cb33245f26d2b2c1
ego4L <152010822+ego4L@users.noreply.github.com>
2025-12-01 22:38:02 +0100

    Initial commit

---
+++ src/main/java/com/egoxide/finance/gcpstockrevenue/GcpStockRevenueApplication.java
@@ -0,0 +1,13 @@
+package com.egoxide.finance.gcpstockrevenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpStockRevenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpStockRevenueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egoxide/finance/gcpstockrevenue/GcpStockRevenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egoxide.finance.gcpstockrevenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpStockRevenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
